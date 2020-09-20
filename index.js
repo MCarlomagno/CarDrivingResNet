bee1a0e4890b3b15b4411b6b0fe41c45344c12a7
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2020-09-20 16:18:08 -0300

    feat :boom: posenet model added & video + canva on screen

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-var play;
+import * as posenet from '@tensorflow-models/posenet';
+import dat from 'dat.gui';
+import Stats from 'stats.js';
+import {drawBoundingBox, drawKeypoints, drawSkeleton, isMobile, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './utils';
+
+let play;
+const videoWidth = 400;
+const videoHeight = 300;
+const stats = new Stats();
+
 var loaded = false;
 
-function done(){
-    document.getElementById("ldc").innerHTML="";
-    document.getElementById("ldc").style.display = "none";
-    document.getElementById("playBtn").innerHTML = "PLAY";
-    loaded = true;
-}
+const defaultQuantBytes = 2;
+
+const defaultResNetMultiplier = 1.0;
+const defaultResNetStride = 32;
+const defaultResNetInputResolution = 250;
+
+
+const guiState = {
+    algorithm: 'single-pose',
+    input: {
+      architecture: 'ResNet50',
+      outputStride: defaultResNetStride,
+      inputResolution: defaultResNetInputResolution,
+      multiplier: defaultResNetMultiplier,
+      quantBytes: defaultQuantBytes
+    },
+    singlePoseDetection: {
+      minPoseConfidence: 0.1,
+      minPartConfidence: 0.5,
+    },
+    multiPoseDetection: {
+      maxPoseDetections: 5,
+      minPoseConfidence: 0.15,
+      minPartConfidence: 0.1,
+      nmsRadius: 30.0,
+    },
+    output: {
+      showVideo: true,
+      showSkeleton: true,
+      showPoints: false,
+      showBoundingBox: false,
+    },
+    net: null,
+  };
 
 (function(){
     var w = window.innerWidth || 360;
@@ -414,6 +451,150 @@ play = function(){
     }
 }
 })();
+
+/**
+ * Feeds an image to posenet to estimate poses - this is where the magic
+ * happens. This function loops with a requestAnimationFrame method.
+ */
+function detectPoseInRealTime(video, net) {
+    const canvas = document.getElementById('output');
+    const ctx = canvas.getContext('2d');
+  
+    // since images are being fed from a webcam, we want to feed in the
+    // original image and then just flip the keypoints' x coordinates. If instead
+    // we flip the image, then correcting left-right keypoint pairs requires a
+    // permutation on all the keypoints.
+    const flipPoseHorizontal = true;
+  
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+  
+    async function poseDetectionFrame() {
+  
+      // Begin monitoring code for frames per second
+      stats.begin();
+  
+      let poses = [];
+      let minPoseConfidence;
+      let minPartConfidence;
+
+      // single pose
+      const pose = await net.estimatePoses(video, {
+        flipHorizontal: flipPoseHorizontal,
+        decodingMethod: 'single-person'
+      });
+      poses = poses.concat(pose);
+      minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
+      minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
+  
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+  
+      if (guiState.output.showVideo) {
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.translate(-videoWidth, 0);
+        ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+        ctx.restore();
+      }
+  
+      // For each pose (i.e. person) detected in an image, loop through the poses
+      // and draw the resulting skeleton and keypoints if over certain confidence
+      // scores
+      poses.forEach(({score, keypoints}) => {
+        if (score >= minPoseConfidence) {
+          if (guiState.output.showPoints) {
+            drawKeypoints(keypoints, minPartConfidence, ctx);
+          }
+          if (guiState.output.showSkeleton) {
+            drawSkeleton(keypoints, minPartConfidence, ctx);
+          }
+          if (guiState.output.showBoundingBox) {
+            drawBoundingBox(keypoints, ctx);
+          }
+        }
+      });
+  
+      // End monitoring code for frames per second
+      stats.end();
+  
+      requestAnimationFrame(poseDetectionFrame);
+    }
+    poseDetectionFrame();
+}
+
+async function done(){
+
+    /// loads PoseNet NN
+    const net = await posenet.load({
+        architecture: guiState.input.architecture,
+        outputStride: guiState.input.outputStride,
+        inputResolution: guiState.input.inputResolution,
+        multiplier: guiState.input.multiplier,
+        quantBytes: guiState.input.quantBytes
+    });
+  
+    let video;
+    try {
+        video = await loadVideo();
+
+        
+        // setupGui([], net);
+        // setupFPS();
+        detectPoseInRealTime(video, net);
+
+        document.getElementById("ldc").innerHTML="";
+        document.getElementById("ldc").style.display = "none";
+        document.getElementById("playBtn").innerHTML = "PLAY";
+    } catch (e) {
+        let info = document.getElementById('info');
+        info.textContent = 'this browser does not support video capture,' +
+            'or this device does not have a camera';
+        info.style.display = 'block';
+        throw e;
+    }
+
+    loaded = true;
+}
+
+
+async function loadVideo() {
+    
+    const video = await setupCamera();
+    video.play();
+    return video;
+  }
+
+async function setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error(
+          'Browser API navigator.mediaDevices.getUserMedia not available');
+    }
+  
+    const video = document.getElementById('video');
+    video.width = videoWidth;
+    video.height = videoHeight;
+  
+    const mobile = isMobile();
+    const stream = await navigator.mediaDevices.getUserMedia({
+      'audio': false,
+      'video': {
+        facingMode: 'user',
+        width: mobile ? undefined : videoWidth,
+        height: mobile ? undefined : videoHeight,
+      },
+    });
+
+    console.log('before setup video');
+    video.srcObject = stream;
+    console.log('after setup video');
+
+  
+    return new Promise((resolve) => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+  }
 
 /// template event listeners
 document.getElementById('body').onload = done;
